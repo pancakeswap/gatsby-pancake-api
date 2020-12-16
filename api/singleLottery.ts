38ecc5e46713ea9ce2a95d8b56a35691cfc4a921
Philip Kirchner <CrVnsh@users.noreply.github.com>
2020-12-16 10:57:59 +0100

    Update api/singleLottery.ts
    Co-authored-by: ChefKai <74599990+ChefKai@users.noreply.github.com>

--- api/singleLottery.ts
+++ api/singleLottery.ts
@@ -1,4 +1,4 @@
-import { NowRequest, NowResponse } from "@now/node";
+import { NowRequest, NowResponse } from "@vercel/node";
 import { getContract } from "../lib/contract";
 
 import { generateLotteryDate } from "../utils/generateLotteryDate";
